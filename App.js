401245cb46816370e32d68c33019d1d47f5d401a
B00142713 Abel Ikhaguebor <B00142713@mytudublin.ie>
2025-09-22 16:32:50 +0100

    Step 1

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StyleSheet, View, Text } from 'react-native';
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default function App(){ 
+
+  return(
+    <View>
+    <Text>Hello, World</Text>
+    </View>
+  )
+};
+
+  
